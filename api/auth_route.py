e57f405b30bde5374dba1654c81fae541f9a2f9d
hsotnas76 <hsotnas76@gmail.com>
2022-10-21 07:27:48 +0545

    update

--- api/auth_route.py
+++ api/auth_route.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
 from services.auth_service import login_user,register_user
-
 auth_route = Blueprint('auth_route', __name__)
 
 
--- app.py
+++ app.py
@@ -33,47 +33,8 @@ def db_create():
 @app.cli.command('db_drop')
 def db_drop():
     db.drop_all()
-    print('db_drop successfully')    
-    
-    
-# @app.cli.command('db_seed')
-# def db_seed():
-#     mercury = Planet(planet_name='Mercury',
-#                      planet_type= 'Class D',
-#                      home_star = 'Sol',
-#                      mass= 3.258e23,
-#                      radius = 1516,
-#                      distance =35.98e6) 
-    
-#     venus = Planet(planet_name='Venus',
-#                      planet_type= 'Class K',
-#                      home_star = 'Sol',
-#                      mass= 4.867e24,
-#                      radius = 3760,
-#                      distance =67.24e6) 
-    
-#     earth = Planet(planet_name='Earth',
-#                      planet_type= 'Class M',
-#                      home_star = 'Sol',
-#                      mass= 5.972e24,
-#                      radius = 3959,
-#                      distance =92.96e6)   
-    
-#     db.session.add(mercury)
-#     db.session.add(venus)
-#     db.session.add(earth)
-    
-    
-#     test_user = User(first_name='first', last_name='last',
-#                      email='email@example.com',
-#                      password='password'
-#                      )
-    
-#     db.session.add(test_user)
-#     db.session.commit()
-#     print('database seeded')  
-    
-
+    print('db_drop successfully')      
+   
 
 if __name__ == '__main__':
     app.run(debug=True)
